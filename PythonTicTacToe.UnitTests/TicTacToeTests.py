b23e06a233c729e4d8d025e6e5a40bbce2da2f83
Sándor Balázs <it.sandor.balazs@gmail.com>
2018-07-04 23:49:21 +0200

    Tesztek hozzáadása.

---
+++ PythonTicTacToe.UnitTests/TicTacToeTests.py
@@ -0,0 +1,12 @@
+import unittest
+
+class EmptyTest(unittest.TestCase):
+    def test_empty(self):
+        self.assertTrue(True)
+
+class TicTacToeTests(unittest.TestCase):
+    def test_empty(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
